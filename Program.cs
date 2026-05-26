167e689f0b2aba8300c80c8ccfd972a6a990ae81
Lucia Isabel Ponce <luciaponce280606@gmail.com>
2026-05-26 09:53:16 -0300

    calc1

--- Program.cs
+++ Program.cs
@@ -19,3 +19,35 @@ if (int.TryParse(ingresado,out numero) && numero > 0)
     }
     Console.WriteLine("Valor convertido: " + invertido);
 }
+
+    string op = "1";
+    int operacion;
+    while(int.TryParse(op,out operacion) && operacion!=0) {
+    Console.WriteLine("Ingrese un numero a: ");
+    string primer = Console.ReadLine();
+    Console.WriteLine("Ingrese un numero b: ");
+    string segundo = Console.ReadLine();
+    int numeroa, numerob, resultado=0;
+    Console.WriteLine("Ingrese la operacion que quiere realizar segun el menu \n 1: suma \n 2: resta \n 3: multiplicacion \n 4: division \n 0: fin de operaciones ");
+    op=Console.ReadLine();
+    if (int.TryParse(primer,out numeroa) && int.TryParse(segundo, out numerob) && int.TryParse(op,out operacion))
+    {
+    
+        switch (operacion)
+            {
+                case 1:
+                resultado = numeroa + numerob;
+                break;
+                case 2: 
+                    resultado = numeroa-numerob;
+                break;
+                case 3: 
+                    resultado = numeroa * numerob;
+                break;
+                case 4: 
+                    resultado = numeroa / numerob;
+                break;
+            }
+            Console.WriteLine("Resultado: " + resultado);
+        }
+    };
