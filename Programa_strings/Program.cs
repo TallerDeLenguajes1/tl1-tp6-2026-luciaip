6b0a5295e9dbc394ae4829f445ef9979a7d9e6a0
Lucia Isabel Ponce <luciaponce280606@gmail.com>
2026-06-08 11:53:30 -0300

    Calculadorafinal

---
+++ Programa_strings/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ingrese una cadena de texto: ");
+string? cadena_2;
+string? cadena;
+cadena = Console.ReadLine();
+int longitud = cadena.Length;
+cadena_2 = Console.ReadLine();
+string? concatenada= string.Concat(cadena, cadena_2);
